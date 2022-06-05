777252811927ae1b6864b2633c5121ab72a58402
Martin <m.mrugala@student.mini.pw.edu.pl>
2022-06-06 00:10:16 +0200

    add cirq to gui

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,7 @@
 import ipywidgets as widgets
 from algorithms.shor import *
 from IPython.display import display, Markdown, clear_output
+from cirq_shor import find_factor_cirq, naive_order_finder, quantum_order_finder
 
 factoring_number = widgets.IntText(
     value=15,
@@ -21,15 +22,24 @@ def on_button_clicked(_):
 
         factor_list1 = find_factor(factoring_number.value, a=7, qubits_count=8, timeout = timeout.value)
         factor_list2 = find_factor(factoring_number.value, a=7, qubits_count=8, timeout = timeout.value)
+        factor_list3 = find_factor_cirq(factoring_number.value, naive_order_finder, False)
+        factor_list4 = find_factor_cirq(factoring_number.value, quantum_order_finder, False)
         if len(factor_list1) == 0:
-            display(Markdown(f"TIMEOUT: no factor found for the first run"))
+            display(Markdown(f"Qiskit - TIMEOUT: no factor found for the first run"))
         else:
-            display(Markdown(f"Found factors first run: {factor_list1}"))
+            display(Markdown(f"Qiskit - Found factors first run: {factor_list1}"))
         if len(factor_list2) == 0:
-            display(Markdown(f"TIMEOUT: no factor found for the second run"))
+            display(Markdown(f"Qiskit - TIMEOUT: no factor found for the second run"))
         else:
-            display(Markdown(f"Found factors second run: {factor_list2}"))
-
+            display(Markdown(f"Qiskit - Found factors second run: {factor_list2}"))
+        if factor_list3 is None:
+            display(Markdown(f"Cirq - TIMEOUT: no factor found for the naive run"))
+        else:
+            display(Markdown(f"Cirq - Found factors naive run: {factor_list3}"))
+        if factor_list4 is None:
+            display(Markdown(f"Cirq - TIMEOUT: no factor found for the quantum run"))
+        else:
+            display(Markdown(f"Cirq - Found factors quantum run: {factor_list4}"))
 
 button.on_click(on_button_clicked)
 
